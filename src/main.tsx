8d372a9a5a487f00fa9389826983ffd43727ac83
Ahmed Faraj <ahmedfrag4040@gmail.com>
2025-10-20 19:50:44 +0300

    Add Vite React setup, index.html, .gitignore and Pages workflow

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import Portfolio from '../app.tsx'
+
+const container = document.getElementById('root')!
+const root = createRoot(container)
+root.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>
+)
+
+
